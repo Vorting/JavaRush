8f857545bd2ce224f8c61847a767783d76bb0016
Dima Ivanov <ya.warkus@gmail.com>
2019-05-29 11:41:18 +0300

    lvl№2

---
+++ src/lvl2/Solution.java
@@ -0,0 +1,18 @@
+package lvl2;
+
+public class Solution {
+    public static void main(String[] args) {
+        print("Java is easy to learn!");
+        print("Java opens many opportunities!");
+
+    }
+
+
+    public static void print(String s) {
+        int count = 4;
+        while (count != 0) {
+            System.out.println(s);
+            count--;
+        }
+    }
+}
