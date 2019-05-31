7d246f0afd02bcfe84fb5122a95aa574cfbaa2d6
Dima Ivanov <ya.warkus@gmail.com>
2019-05-31 16:27:37 +0300

    minV3Demo added

---
+++ src/JavaSyntax/lecture8/MinV3Demo.java
@@ -0,0 +1,26 @@
+package JavaSyntax.lecture8;
+
+public class MinV3Demo {
+    public static void main(String[] args) {
+
+        System.out.println(min(-20, -10));
+        System.out.println(min(-40, -10, -30, 40));
+        System.out.println(min(-20, -40, -30, 40));
+        System.out.println(min(-20, -10, -40, 40));
+        System.out.println(min(-20, -10, -30, -40));
+    }
+
+    public static int min(int a, int b) {
+
+        if (a < b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public static int min(int a, int b, int c, int d) {
+
+        return min(a, b) < min(c, d) ? min(a, b) : min(c, d);
+    }
+}
