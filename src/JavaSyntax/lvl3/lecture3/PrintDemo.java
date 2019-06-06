296feae94a5a66097b6a18306673ddc8cb038875
vorting <ya.warkus@gmail.com>
2019-06-06 14:23:05 +0300

    lvl3

---
+++ src/JavaSyntax/lvl3/lecture3/PrintDemo.java
@@ -0,0 +1,12 @@
+package JavaSyntax.lvl3.lecture3;
+
+public class PrintDemo {
+    public static void main(String[] args) {
+        printString("Hello, Amigo!");
+    }
+
+
+    public static void printString(String someWord) {
+        System.out.println(someWord);
+    }
+}
