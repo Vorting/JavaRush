b36ae5632eb5633026b7ec639f9e3dcd82b97728
Dima Ivanov <ya.warkus@gmail.com>
2019-06-26 12:47:27 +0300

    add AddCatDemo

---
+++ src/JavaSyntax/lvl4/AddCatDemo.java
@@ -0,0 +1,18 @@
+package JavaSyntax.lvl4;
+
+public class AddCatDemo {
+    private static int catsCount = 0;
+
+    public static void main(String[] args) {
+        AddCatDemo cat = new AddCatDemo();
+        System.out.println("Размер котика до увеличения " + catsCount);
+        System.out.println("Размер котика после увеличения ");
+        addNewCat();
+        System.out.println(catsCount);
+
+    }
+
+    public static void addNewCat() {
+        catsCount++;
+    }
+}
