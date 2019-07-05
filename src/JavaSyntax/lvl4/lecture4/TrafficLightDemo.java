aca5691fb17267a22a2d4ebadb586c807b80b752
Dima Ivanov <ya.warkus@gmail.com>
2019-07-05 12:10:38 +0300

    solved TrafficLightDemo

--- src/JavaSyntax/lvl4/lecture4/TrafficLightDemo.java
+++ src/JavaSyntax/lvl4/lecture4/TrafficLightDemo.java
@@ -1,60 +1,27 @@
 package JavaSyntax.lvl4.lecture4;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+
 
 public class TrafficLightDemo {
-    private static double third_minute = 3;
-    private static double fourth_minute = 3;
-    private static double fifth_minute = 5;
-
 
     public static void main(String[] args) throws IOException {
 
-//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-//        String hours = reader.readLine();
-//        double time = Double.parseDouble(hours);
-//
-//        guessColor(time);
-//
-//
-//    }
-//
-//
-//    public static double guessColor(double time_in_minutes) {
-//        double tmp = 0;
-//
-//        tmp = time_in_minutes / fifth_minute;
-//        if (tmp == fourth_minute) {
-//            System.out.println(tmp);
-//            System.out.println("зеленый");
-//        } else if (tmp ==)
-//
-//
-//            return tmp;
-        Scanner scanner = new Scanner(System.in);
-        double t = scanner.nextDouble();
-        int a = (int) (t / 5);
-        int e = (int) t;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String hours = reader.readLine();
+        double time = Double.parseDouble(hours);
 
-        int d = e - a * 5;
-        switch (d) {
-            case 0:
+        double number_after_comma = time % 5;
 
-            case 1:
-            case 5:
-
-            case 2:
-                System.out.println("зелёный");
-                break;
-            case 3:
-                System.out.println("жёлтый");
-                break;
-            case 4:
-                System.out.println("красный");
-                break;
-
+        if (number_after_comma >=0 && number_after_comma<3){
+            System.out.println("зеленый");
+        }else if (number_after_comma>=3 && number_after_comma<4){
+            System.out.println("желтый");
+        }else if (number_after_comma >=4 && number_after_comma<5){
+            System.out.println("красный");
         }
-
     }
+
 }
