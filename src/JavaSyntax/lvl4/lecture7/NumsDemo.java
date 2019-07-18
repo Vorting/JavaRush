46ff2e50e04b9c9bf2d87eedc8c13aee1ed6b734
Dima Ivanov <ya.warkus@gmail.com>
2019-07-18 12:12:42 +0300

    added NumsDemo

--- src/JavaSyntax/lvl4/lecture7/NumsDemo.java
+++ src/JavaSyntax/lvl4/lecture7/NumsDemo.java
@@ -8,32 +8,37 @@ public class NumsDemo {
 
 
     public static void main(String[] args) throws IOException {
-        int number;
+
         int counter = 0;
 
-
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        number = Integer.parseInt(reader.readLine());
 
-        while (number != 0) {
-            counter += 1;
-            number /= 10;
+        int numbers = Integer.parseInt(reader.readLine());
+
+        int tmp = numbers;
+        while (tmp != 0) {
+            counter++;
+            tmp /= 10;
         }
 
-        if (counter == 1 && (number % 2 == 0)) {
-            System.out.println("четное однозначное число");
+        if (numbers > 0 && numbers < 1000) {
+            if (counter == 1 && numbers % 2 == 0) {
+                System.out.println("четное однозначное число");
+            } else if (counter == 1 && numbers % 2 != 0){
+                System.out.println("нечетное однозначное число");
+            }
+            else if (counter == 2 && numbers % 2 == 0) {
+                System.out.println("четное двузначное число");
+            } else if (counter == 2 && numbers % 2 != 0){
+                System.out.println("нечетное двузначное число");
+            }
+            else if (counter == 3 && numbers % 2 == 0) {
+                System.out.println("четное трехзначное число");
+            } else if (counter == 3 && numbers % 2 != 0){
+                System.out.println("нечетное трехзначное число");
+            }
         } else {
-            System.out.println("нечетное однозначное число");
-        }
-        if (counter == 2 && (number % 2 == 0)) {
-            System.out.println("четное двузначное число");
-        } else {
-            System.out.println("нечетное двузначное число");
-        }
-        if (counter == 3 && (number % 2 == 0)) {
-            System.out.println("четное трехзначное число");
-        } else {
-            System.out.println("нечетное трехзначное число");
+            System.out.print("");
         }
     }
 }
