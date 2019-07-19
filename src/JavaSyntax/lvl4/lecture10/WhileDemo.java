9afa30cd60a2e7d87fa062843cdfdb8f84bea048
Dima Ivanov <ya.warkus@gmail.com>
2019-07-19 15:57:50 +0300

    create WhileDemo

---
+++ src/JavaSyntax/lvl4/lecture10/WhileDemo.java
@@ -0,0 +1,12 @@
+package JavaSyntax.lvl4.lecture10;
+
+public class WhileDemo {
+    public static void main(String[] args) {
+        boolean stop = false;
+        int i=0;
+        while (!stop) {
+            i++;
+            if (i==10)
+        }
+    }
+}
