07c8335009b9be8a6ca6ff7945dd9e4cffa92724
vorting <ya.warkus@gmail.com>
2019-07-20 22:13:49 +0300

    WhileDecreasDemo

---
+++ src/JavaSyntax/lvl4/lecture10/WhileDecreaseDemo.java
@@ -0,0 +1,10 @@
+package JavaSyntax.lvl4.lecture10;
+
+public class WhileDecreaseDemo {
+    public static void main(String[] args) {
+        int start = 10;
+        while (start != 0) {
+            System.out.println(start--);
+        }
+    }
+}
