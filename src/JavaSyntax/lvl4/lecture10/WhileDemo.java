f6604f23e6a37aacb6a7e0999ed24f310435d860
vorting <ya.warkus@gmail.com>
2019-07-20 22:09:12 +0300

    WhileDemo

---
+++ src/JavaSyntax/lvl4/lecture10/WhileDemo.java
@@ -0,0 +1,10 @@
+package JavaSyntax.lvl4.lecture10;
+
+public class WhileDemo {
+    public static void main(String[] args) {
+        int start = 1;
+        while (start != 11) {
+            System.out.println(start++);
+        }
+    }
+}
---
+++ src/JavaSyntax/lvl4/lecture7/ThreePositiveDemo.java
@@ -0,0 +1,29 @@
+package JavaSyntax.lvl4.lecture7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ThreePositiveDemo {
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int number1 = Integer.parseInt(reader.readLine());
+        int number2 = Integer.parseInt(reader.readLine());
+        int number3 = Integer.parseInt(reader.readLine());
+
+        if ((number1 > 0 && number2 > 0 && number3 <= 0)) {
+            System.out.println(2);
+        }
+        if ((number2 > 0 && number3 > 0) && number1 <= 0) {
+            System.out.println(2);
+        }
+        if ((number3 > 0 && number1 > 0) && number2 <= 0) {
+            System.out.println(2);
+        }
+        if (number1 < 0 && number2 < 0 && number3 < 0) {
+            System.out.println(0);
+        }
+    }
+}
