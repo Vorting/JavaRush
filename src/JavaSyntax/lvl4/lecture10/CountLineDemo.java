8d922a3859ea04198b80988e6084bafb9242b8d0
vorting <ya.warkus@gmail.com>
2019-07-20 22:39:31 +0300

    add CountLineDemo

---
+++ src/JavaSyntax/lvl4/lecture10/CountLineDemo.java
@@ -0,0 +1,19 @@
+package JavaSyntax.lvl4.lecture10;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class CountLineDemo {
+    public static void main(String[] args) {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = reader.readLine();
+        int N = Integer.parseInt(reader.readLine());
+
+
+        while (N != 0) {
+            System.out.println(line);
+            N--;
+        }
+    }
+}
