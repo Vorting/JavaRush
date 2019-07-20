2463fc5542e263b309b85706684e2db2b406434a
vorting <ya.warkus@gmail.com>
2019-07-20 22:11:09 +0300

    cleanup

--- src/JavaSyntax/lvl4/lecture10/WhileDemo.java
+++ src/JavaSyntax/lvl4/lecture10/WhileDemo.java
@@ -2,17 +2,10 @@ package JavaSyntax.lvl4.lecture10;
 
 public class WhileDemo {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         int start = 1;
         while (start != 11) {
             System.out.println(start++);
-=======
-        boolean stop = false;
-        int i=0;
-        while (!stop) {
-            i++;
-            if (i==10)
->>>>>>> 9afa30cd60a2e7d87fa062843cdfdb8f84bea048
         }
     }
 }
