aa53e3ad8920405657843b7fdf9c3514711881e3
Dima Ivanov <ya.warkus@gmail.com>
2019-07-22 11:41:50 +0300

    complete While_S_Demo

--- src/JavaSyntax/lvl4/lecture10/While_S_Demo.java
+++ src/JavaSyntax/lvl4/lecture10/While_S_Demo.java
@@ -2,16 +2,19 @@ package JavaSyntax.lvl4.lecture10;
 
 public class While_S_Demo {
     public static void main(String[] args) {
-        String l = "S";
-        int start = 0;
-        while (start != 10) {
-            start++;
-            int finish = start;
-            while (finish == start) {
-                System.out.print("S");
-                finish--;
 
+        int i = 0;
+        while (i < 10) {
+            i++;
+            int t = 0;
+            while (t < 10) {
+                if (t == 10) {
+                    break;
+                }
+                System.out.print("S");
+                t++;
             }
+            System.out.print("\n");
         }
     }
 }
