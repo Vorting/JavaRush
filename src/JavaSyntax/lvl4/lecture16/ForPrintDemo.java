bf61a40ad2a125edd7f7d7b926f055e431e82b98
Dima Ivanov <ya.warkus@gmail.com>
2019-07-25 14:46:46 +0300

    add ForPrintDemo

---
+++ src/JavaSyntax/lvl4/lecture16/ForPrintDemo.java
@@ -0,0 +1,9 @@
+package JavaSyntax.lvl4.lecture16;
+
+public class ForPrintDemo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Я никогда не буду работать за копейки. Амиго");
+        }
+    }
+}
