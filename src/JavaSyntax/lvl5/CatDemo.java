b29c30f1c71620115e94fe849a776cdb14276f77
vorting <ya.warkus@gmail.com>
2019-07-28 12:49:31 +0300

    add level5

---
+++ src/JavaSyntax/lvl5/CatDemo.java
@@ -0,0 +1,13 @@
+package JavaSyntax.lvl5;
+
+public class CatDemo {
+
+    private String name;
+    private int age;
+    private int weight;
+    private int strength;
+
+    public static void main(String[] args) {
+
+    }
+}
