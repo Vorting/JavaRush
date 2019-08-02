4b90f36f470f533a3497402aa82eb871435e5327
vorting <ya.warkus@gmail.com>
2019-08-02 20:35:53 +0300

    cleaning up

--- src/JavaSyntax/lvl5/lecture9/Rectangle2Demo.java
+++ src/JavaSyntax/lvl5/lecture9/Rectangle2Demo.java
@@ -9,11 +9,27 @@ public class Rectangle2Demo {
     private int width;
     private int height;
 
-    public Rectangle2Demo(){
-
+    public Rectangle2Demo(int top) {
+        this.top = top;
     }
 
+    public Rectangle2Demo(int top, int left) {
+        this.top = top;
+        this.left = left;
+    }
 
+    public Rectangle2Demo(int top, int left, int width) {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+    }
+
+    public Rectangle2Demo(int top, int left, int width, int height) {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = height;
+    }
 
     public static void main(String[] args) {
 
