cd689317653d8fe0b5476c2e1cbb109dfda16b06
Dima Ivanov <ya.warkus@gmail.com>
2019-08-15 14:12:49 +0300

    solved

--- src/JavaSyntax/lvl5/lecture9/Circle2Demo.java
+++ src/JavaSyntax/lvl5/lecture9/Circle2Demo.java
@@ -5,25 +5,25 @@ public class Circle2Demo {
     public double y;
     public double radius;
 
-    public Circle2Demo() {
-    }
-
-    public Circle2Demo(double x) {
-        this.x = x;
-    }
-
-    public Circle2Demo(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-
     public Circle2Demo(double x, double y, double radius) {
         this.x = x;
         this.y = y;
         this.radius = radius;
     }
 
-    public static void main(String[] args) {
+    public Circle2Demo(double x, double y) {
 
+        this(x, y, 10);
+    }
+
+    public Circle2Demo() {
+        this(5, 5, 1);
+    }
+
+    public static void main(String[] args) {
+        Circle2Demo circle = new Circle2Demo();
+        System.out.println(circle.x + " " + circle.y + " " + circle.radius);
+        Circle2Demo anotherCircle = new Circle2Demo(10, 5);
+        System.out.println(anotherCircle.x + " " + anotherCircle.y + " " + anotherCircle.radius);
     }
 }
