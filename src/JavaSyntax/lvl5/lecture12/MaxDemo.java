3877c0121fb1e784639830fc46b91d22beed7606
Dima Ivanov <ya.warkus@gmail.com>
2019-08-19 17:05:00 +0300

    add MaxDemo

---
+++ src/JavaSyntax/lvl5/lecture12/MaxDemo.java
@@ -0,0 +1,22 @@
+package JavaSyntax.lvl5.lecture12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MaxDemo {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        int maximum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int a = Integer.parseInt(reader.readLine());
+            maximum = i;
+            if (a > maximum) {
+                maximum = a;
+            }
+        }
+        System.out.println(maximum);
+    }
+}
--- src/JavaSyntax/lvl5/lecture12/MinDemo.java
+++ src/JavaSyntax/lvl5/lecture12/MinDemo.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class MinMaxDemo {
+public class MinDemo {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(reader.readLine());
