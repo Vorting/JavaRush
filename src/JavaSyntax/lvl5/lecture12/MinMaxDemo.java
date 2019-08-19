e3318ba82eb6d4f04a870b6d154e68c15072b514
Dima Ivanov <ya.warkus@gmail.com>
2019-08-19 15:23:26 +0300

    add MinMaxDemo

---
+++ src/JavaSyntax/lvl5/lecture12/MinMaxDemo.java
@@ -0,0 +1,23 @@
+package JavaSyntax.lvl5.lecture12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MinMaxDemo {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
+        int e = Integer.parseInt(reader.readLine());
+
+        int minimum = min(a, b, c, d, e);
+        System.out.println("Minimum = " + minimum);
+    }
+
+    private static int min(int a, int b, int c, int d, int e) {
+        return (a <= b) ? ((c <= d) ? c : ((e <= a) ? e : a)) : ((a < c) ? a : (b < c) ? b : c);
+    }
+}
