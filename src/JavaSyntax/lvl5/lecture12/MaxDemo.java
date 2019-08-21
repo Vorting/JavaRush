0aa8c0731e5c724dc7280bc0c90e10a4376cc7da
Dima Ivanov <ya.warkus@gmail.com>
2019-08-21 12:14:55 +0300

    MaxDemo solved

--- src/JavaSyntax/lvl5/lecture12/MaxDemo.java
+++ src/JavaSyntax/lvl5/lecture12/MaxDemo.java
@@ -8,22 +8,17 @@ public class MaxDemo {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
+        int maximum = 0;
         if (n > 0) {
-            int maximum = 0;
-            int minimum = 0;
             for (int i = 0; i < n; i++) {
                 int a = Integer.parseInt(reader.readLine());
-                maximum = a;
-                if (a > maximum) {
+                if (i == 0) {
                     maximum = a;
-                } else if (a < minimum) {
-                    minimum = a;
-                    System.out.println(minimum);
+                } else if (a > maximum) {
+                    maximum = a;
                 }
             }
-            System.out.println("max = " + maximum + ", min = " + minimum);
-        } else {
-            System.out.println("n меньше 0, выходим...");
         }
+        System.out.println(maximum);
     }
 }
