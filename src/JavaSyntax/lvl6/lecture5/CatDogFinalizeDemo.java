831402a1ed6f9408ec8bbc9e88e44372d0ae8b89
Dima <ya.warkus@gmail.com>
2019-08-22 22:36:47 +0300

    add CatDogFinalizeDemo

--- src/JavaSyntax/lvl6/lecture5/CatDogFinalizeDemo.java
+++ src/JavaSyntax/lvl6/lecture5/CatDogFinalizeDemo.java
@@ -4,17 +4,13 @@ public class CatDogFinalizeDemo {
 
     public static void main(String[] args) throws Throwable {
 
-        Cat2 cat = new Cat2();
-        Dog2 dog = new Dog2();
 
-        for (int i = 0; i < 20_000; i++) {
-            System.out.println(i);
-            cat.finalize();
+        for (int i = 0; i < 50_000; i++) {
+
+            Cat2 cat = new Cat2();
+            Dog2 dog = new Dog2();
         }
-        for (int j = 0; j < 50_000; j++) {
-            System.out.println(j);
-            dog.finalize();
-        }
+
     }
 }
 
@@ -32,4 +28,61 @@ class Dog2 {
         super.finalize();
         System.out.println("A Dog was destroyed");
     }
+
+
+//==================================
+//int count = 0;
+//    Cat cat = new Cat(0);
+//    Dog dog = new Dog(0);
+//        for (count = 1; count < 50_000; count++) {
+//        cat.generator(count);
+//        dog.generator(count);
+//    }
+//}
+//}
+//
+//class Cat {
+//    private int x;
+//    private String name;
+//
+//    public Cat(String name) {
+//        this.name = name;
+//    }
+//
+//    public Cat(int x) {
+//        this.x = x;
+//    }
+//
+//    @Override
+//    protected void finalize() throws Throwable {
+//        super.finalize();
+//        System.out.println("A Cat was destroyed");
+//    }
+//
+//    void generator(int i) {
+//        Cat c = new Cat(i);
+//    }
+//}
+//
+//class Dog {
+//    private int x;
+//    private String name;
+//
+//    public Dog(String name) {
+//        this.name = name;
+//    }
+//
+//    public Dog(int x) {
+//        this.x = x;
+//    }
+//
+//    @Override
+//    protected void finalize() throws Throwable {
+//        super.finalize();
+//        System.out.println("A Dog was destroyed");
+//    }
+//
+//    void generator(int i) {
+//        Dog d = new Dog(i);
+//    }    
 }
