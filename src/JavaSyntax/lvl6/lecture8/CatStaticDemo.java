e9920ae4956a1190298baae18c32c8e140fcd10d
Dima Ivanov <ya.warkus@gmail.com>
2019-08-27 10:25:40 +0300

    add lecture8

---
+++ src/JavaSyntax/lvl6/lecture8/CatStaticDemo.java
@@ -0,0 +1,14 @@
+package JavaSyntax.lvl6.lecture8;
+
+public class CatStaticDemo {
+    private static int catCount = 0;
+
+
+    public CatStaticDemo() {
+        catCount++;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
