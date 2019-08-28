e07e3a255ac3116234419f3cc3393c08c72a6966
Dima Ivanov <ya.warkus@gmail.com>
2019-08-28 17:07:56 +0300

    Add_3_namesDemo

---
+++ src/JavaSyntax/lvl6/lecture11/Add_3_namesDemo.java
@@ -0,0 +1,17 @@
+package JavaSyntax.lvl6.lecture11;
+
+public class Add_3_namesDemo {
+    public static String name;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class Cat {
+        public static String name;
+    }
+
+    public static class Dog {
+        public static String name;
+    }
+}
