be81c5e4d70ca3155c942020622b70fb3226332f
Dima Ivanov <ya.warkus@gmail.com>
2019-08-28 15:09:17 +0300

    add CalculatorDemo

---
+++ src/JavaSyntax/lvl6/lecture8/CalculatorDemo.java
@@ -0,0 +1,28 @@
+package JavaSyntax.lvl6.lecture8;
+
+public class CalculatorDemo {
+    public static void main(String[] args) {
+
+    }
+
+
+    public static int plus(int a, int b) {
+        return a + b;
+    }
+
+    public static int minus(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double division(int a, int b) {
+        return (double) a / (double) b;
+    }
+
+    public static double percent(int a, int b) {
+        return ((double) a / 100) * (double) b;
+    }
+}
