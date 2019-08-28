aea88012b765a376e57086bb1347c7341da9fd12
Dima <ya.warkus@gmail.com>
2019-08-28 22:04:29 +0300

    add StaticsDemo

---
+++ src/JavaSyntax/lvl6/lecture11/StaticsDemo.java
@@ -0,0 +1,33 @@
+package JavaSyntax.lvl6.lecture11;
+
+public class StaticsDemo {
+
+    public static int step;
+
+    public static void main(String[] args) {
+        method1();
+    }
+
+    public static void method1() {
+        method2();
+    }
+
+    public static void method2() {
+        new StaticsDemo().method3();
+    }
+
+    public void method3() {
+        method4();
+    }
+
+    public void method4() {
+        step++;
+        for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
+            System.out.println(element);
+            if (step > 1) {
+            }
+            return;
+            main(null);
+        }
+    }
+}
