faf7813ea5cdfca67aa5538c4e35bf76bb776d7c
Dima Ivanov <ya.warkus@gmail.com>
2019-08-28 17:04:04 +0300

    add two classes KissMyAss & Solution

---
+++ src/JavaSyntax/lvl6/lecture11/KissMyAssDemo.java
@@ -0,0 +1,13 @@
+package JavaSyntax.lvl6.lecture11;
+
+public class KissMyAssDemo {
+
+    public static void main(String[] args) {
+        KissMyShinyMetalAss kissMyAss = new KissMyShinyMetalAss();
+        System.out.println(kissMyAss);
+    }
+
+    public static class KissMyShinyMetalAss {
+
+    }
+}
