c2fbf793968ded68ca23d0b3a184e75a1c4d23a3
Dima Ivanov <ya.warkus@gmail.com>
2019-09-11 12:32:39 +0300

    add ListDemo

--- src/JavaSyntax/lvl7/lecture7/ListDemo.java
+++ src/JavaSyntax/lvl7/lecture7/ListDemo.java
@@ -4,19 +4,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
-public class ArrayListDemo {
+public class ListDemo {
     public static void main(String[] args) throws IOException {
-        ArrayList<String> arrayList = new ArrayList<String>();
+        List<String> arrayList = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-
         for (int i = 0; i < 10; i++) {
+            arrayList.add(i, "Test");
+        }
+
+        for (int j = 0; j < arrayList.size(); j++) {
             arrayList.add(0, reader.readLine());
         }
 
-        for (String x : arrayList) {
-            System.out.println(x);
+        for (String s : arrayList) {
+            System.out.println(s);
         }
+
     }
 }
