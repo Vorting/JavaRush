5e6af4b7db9d356267d3edff2f0d6164bd824e4f
Dima <ya.warkus@gmail.com>
2019-09-16 21:30:43 +0300

    add ArrayListDoubleDemo

---
+++ src/JavaSyntax/lvl7/lecture9/ArrayListDoubleDemo.java
@@ -0,0 +1,23 @@
+package JavaSyntax.lvl7.lecture9;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDoubleDemo {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<String> result = new ArrayList<>();
+        result = doubleValues(reader);
+    }
+
+
+    public static ArrayList<String> doubleValues(ArrayList<String> list) {
+
+
+        return null;
+    }
+
+}
