8469012ec7a922d3f78a15f826cb6ab4ad5e00c7
Dima Ivanov <ya.warkus@gmail.com>
2019-09-25 15:32:39 +0300

    still trying to fix...

--- src/JavaSyntax/lvl7/lecture9/ArrayListHardDemo.java
+++ src/JavaSyntax/lvl7/lecture9/ArrayListHardDemo.java
@@ -9,22 +9,43 @@ import java.util.List;
 public class ArrayListHardDemo {
     public static void main(String[] args) throws IOException {
         List<Integer> mainArray = new ArrayList<>();
-        List<Integer> divByThree = new ArrayList<>();
-        List<Integer> dibByTwo = new ArrayList<>();
-        List<Integer> divByOthers = new ArrayList<>();
-
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
         for (int i = 0; i < 20; i++) {
             mainArray.add(Integer.valueOf(reader.readLine()));
         }
-        
 
+        List<Integer> divByThree = new ArrayList<>();
+        for (int i = 0; i < mainArray.size(); i++) {
+            divByThree.add(i, mainArray.get(i));
+        }
+        System.out.println("divByThree array: ");
+        printList((ArrayList<Integer>) divByThree);
+
+        List<Integer> divByTwo = new ArrayList<>();
+        for (int i = 0; i < mainArray.size(); i++) {
+            divByTwo.add(i, mainArray.get(i));
+        }
+        System.out.println("divByTwo array: ");
+        printList((ArrayList<Integer>) divByTwo);
+
+        List<Integer> divByOthers = new ArrayList<>();
+        for (int i = 0; i < mainArray.size(); i++) {
+            divByOthers.add(i, mainArray.get(i));
+        }
+        System.out.println("divByOthers array: ");
+        printList((ArrayList<Integer>) divByOthers);
     }
 
     public static void printList(ArrayList<Integer> list) {
+
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i) % 3 == 0) {
+                System.out.println(list.get(i));
+            }
+            if (list.get(i) % 2 == 0) {
+                System.out.println(list.get(i));
+
+            } else if ((list.get(i) % 3 != 0) && (list.get(i) % 2 != 0)) {
                 System.out.println(list.get(i));
             }
         }
