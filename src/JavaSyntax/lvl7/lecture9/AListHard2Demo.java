ffe5f35b3d2648ae3258a95312ed41a11cb28958
Dima Ivanov <ya.warkus@gmail.com>
2019-09-30 12:38:11 +0300

    add AlistHard2Demo

---
+++ src/JavaSyntax/lvl7/lecture9/AListHard2Demo.java
@@ -0,0 +1,29 @@
+package JavaSyntax.lvl7.lecture9;
+
+import java.util.ArrayList;
+
+public class AListHard2Demo {
+    public static void main(String[] args) {
+
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("роза");
+        strings.add("лоза");
+        strings.add("лира");
+        strings = fix(strings);
+        for (String s : strings) {
+            System.out.println(s);
+        }
+
+    }
+
+    private static ArrayList<String> fix(ArrayList<String> strings) {
+        for (int i = 0; i < strings.size() - 1; i++) {
+            if (strings.get(i).contains("р")) {
+                strings.remove(0);
+            } else if (strings.get(i).contains("л")) {
+                strings.add(i, strings.get(i));
+            }
+        }
+        return strings;
+    }
+}
