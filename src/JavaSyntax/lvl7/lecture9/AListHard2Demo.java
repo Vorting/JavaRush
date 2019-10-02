50f78d2eafed57f9916adfa11010b49860bdb05d
Dima Ivanov <ya.warkus@gmail.com>
2019-10-02 12:22:05 +0300

    code refactoring at AListHard2Demo

--- src/JavaSyntax/lvl7/lecture9/AListHard2Demo.java
+++ src/JavaSyntax/lvl7/lecture9/AListHard2Demo.java
@@ -4,39 +4,31 @@ import java.util.ArrayList;
 
 public class AListHard2Demo {
     public static void main(String[] args) {
-
-        ArrayList<String> strings = new ArrayList<>();
+        ArrayList<String> strings = new ArrayList<String>();
         strings.add("роза");
         strings.add("лоза");
         strings.add("лира");
         strings = fix(strings);
 
-        for (String s : strings) {
-            System.out.println(s);
+        for (String string : strings) {
+            System.out.println(string);
         }
     }
 
-//    private static ArrayList<String> fix(ArrayList<String> strings) {
-//        for (int i = 0; i < strings.size() - 1; i++) {
-//            if (strings.get(i).contains("р")) {
-//                strings.remove(0);
-//            } else if (strings.get(i).contains("л")) {
-//                strings.add(i, strings.get(i));
-//            }
-//        }
-//        return strings;
-//    }
+    public static ArrayList<String> fix(ArrayList<String> strings) {
+        ArrayList<String> resultStrings = new ArrayList<>();
+        for (String s : strings) {
+            if (s.contains("р") && s.contains("л")) {
+                resultStrings.add(s);
+            } else if (s.contains("р")) {
 
-    private static ArrayList<String> fix(ArrayList<String> strings) {
-        for (int i = 0; i < strings.size() - 1; i++) {
-            if (strings.get(i).contains("р")) {
-                strings.remove(i);
-            }
-            if (strings.get(i).contains("р") && strings.get(i).contains("л")) {
-                strings.add(i, strings.get(i));
-            } else if (!strings.get(i).contains("л") && (!strings.get(i).contains("р"))) {
+            } else if (s.contains("л")) {
+                resultStrings.add(s);
+                resultStrings.add(s);
+            } else {
+                resultStrings.add(s);
             }
         }
-        return strings;
+        return resultStrings;
     }
 }
