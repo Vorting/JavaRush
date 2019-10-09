53b07e6ec8a5369f34b911cc4f03e52115fa299c
Dima Ivanov <ya.warkus@gmail.com>
2019-10-09 15:59:54 +0300

    add ModificatorDemo

---
+++ src/JavaSyntax/lvl7/lecture12/ModificatorDemo.java
@@ -0,0 +1,16 @@
+package JavaSyntax.lvl7.lecture12;
+
+public class ModificatorDemo {
+    public final static int A = 5;
+    public final static int B = 2;
+    public final static int C = A * B;
+
+    public static void main(String[] args) {
+
+    }
+
+    public int getValue() {
+        return C;
+    }
+
+}
