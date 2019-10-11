0dc7dacc48e8ff7dbc26d9df060e3e4b84d333d1
Dima Ivanov <ya.warkus@gmail.com>
2019-10-11 11:46:10 +0300

    solved ArrayListSwitchDemo

--- src/JavaSyntax/lvl7/lecture12/ArrayListSwitchDemo.java
+++ src/JavaSyntax/lvl7/lecture12/ArrayListSwitchDemo.java
@@ -6,27 +6,24 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class ArrayListSwitchDemo {
-    private static int N = 0;
-    private static int M = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         ArrayList<String> arrayList = new ArrayList<>();
 
-        N = Integer.parseInt(reader.readLine());
-        M = Integer.parseInt(reader.readLine());
+        int N = Integer.parseInt(reader.readLine());
+        int M = Integer.parseInt(reader.readLine());
 
         for (int i = 0; i < N; i++) {
-            arrayList.add(i, String.valueOf(N));
-            arrayList.add(i, String.valueOf(M));
+            arrayList.add(String.valueOf(N));
         }
 
-        System.out.println("arrayList before " + arrayList);
-
-        for (int j = 0; j < N; j++) {
-            arrayList.add(M, String.valueOf(arrayList.add(String.valueOf(j))));
+        for (int i = 0; i < M; i++) {
+            arrayList.add(arrayList.size() - 1, arrayList.remove(0));
         }
 
-        System.out.println("arrayList after " + arrayList);
+        for (String s : arrayList) {
+            System.out.println(arrayList);
+        }
     }
 }
