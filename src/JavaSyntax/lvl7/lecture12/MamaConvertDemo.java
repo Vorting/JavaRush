6eb2ad4e942dd963cb6ad7e7d0aa7486b7deb6c4
Dima Ivanov <ya.warkus@gmail.com>
2019-10-16 11:58:38 +0300

    add MamaConverDemo

---
+++ src/JavaSyntax/lvl7/lecture12/MamaConvertDemo.java
@@ -0,0 +1,33 @@
+package JavaSyntax.lvl7.lecture12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class MamaConvertDemo {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> strings = new ArrayList<>();
+        while (true) {
+            String name = reader.readLine();
+            if (name == null || name.isEmpty()) {
+                break;
+            }
+            strings.add(name);
+        }
+        
+        for (String s : convert(strings)) {
+            System.out.println(s);
+        }
+    }
+
+    public static ArrayList<String> convert(ArrayList<String> list) {
+        ArrayList<String> resultString = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            String s = list.get(i);
+            resultString.add(s.toUpperCase());
+        }
+        return resultString;
+    }
+}
