7fea9d7c99e9d2ca28b0e084d856f6fa8884834d
Dima <ya.warkus@gmail.com>
2019-10-16 21:50:39 +0300

    ArrayDecreaseDemo

---
+++ src/JavaSyntax/lvl7/lecture12/ArrayDecreaseDemo.java
@@ -0,0 +1,30 @@
+package JavaSyntax.lvl7.lecture12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class ArrayDecreaseDemo {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[20];
+        for (int i = 0; i < 20; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        for (int x : array) {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(int[] array) {
+
+        for (int i = array.length - 1; i > 0; i--) {
+            array[i] = i;
+        }
+    }
+
+}
