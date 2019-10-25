4d8f8b02d336437495ff244be706439d1003a9a0
Dima Ivanov <ya.warkus@gmail.com>
2019-10-25 17:12:31 +0300

    add MethodsDemo

---
+++ src/JavaSyntax/lvl8/lecture6/MethodsDemo.java
@@ -0,0 +1,33 @@
+package JavaSyntax.lvl8.lecture6;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MethodsDemo {
+    public static void main(String[] args) {
+        List arrayList = new ArrayList();
+        List linkedList = new LinkedList();
+
+    }
+
+    public static List getListForGet() {
+        List arrayList = new ArrayList();
+        return arrayList;
+    }
+
+    public static List getListForSet() {
+        List linkedList = new LinkedList();
+        return linkedList;
+    }
+
+    public static List getListForAddOrInsert() {
+        List arrayList = new ArrayList();
+        return arrayList;
+    }
+
+    public static List getListForRemove() {
+        List arrayList = new ArrayList();
+        return arrayList;
+    }
+}
