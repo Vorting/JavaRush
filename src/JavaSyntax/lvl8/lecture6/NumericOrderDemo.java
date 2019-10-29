6558000a6eb027285246bbc1b2632c8e16939e5f
Dima Ivanov <ya.warkus@gmail.com>
2019-10-29 16:30:29 +0200

    trying to solve

--- src/JavaSyntax/lvl8/lecture6/NumericOrderDemo.java
+++ src/JavaSyntax/lvl8/lecture6/NumericOrderDemo.java
@@ -4,30 +4,44 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class NumericOrderDemo {
-    private static int countOrder = 0;
+    private static int max = 0;
 
     public static void main(String[] args) throws IOException {
         List<Integer> numOrder = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         for (int i = 0; i < 10; i++) {
-            numOrder.add(i, Integer.valueOf(reader.readLine()));
+            numOrder.add(Integer.valueOf(reader.readLine()));
         }
 
-        System.out.println(fillList(numOrder) + " колличество повторяющихся цифр "
-                + countOrder);
-
+        System.out.println(fillList(numOrder) + " колличество повторяющихся цифр " + max);
     }
+
 
     public static List<Integer> fillList(List<Integer> list) {
 
-        Iterator iterator = list.iterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.hasNext());
+
+        ListIterator listIterator = list.listIterator();
+        List<Object> list2 = new ArrayList<>();
+
+        Object current = listIterator.next();
+        Object previous = listIterator.previous();
+        int count = max;
+        int min = 0;
+
+        while (listIterator.hasNext()) {
+            listIterator.hasNext();
+            if (current != previous) {
+                current = listIterator.next();
+                count++;
+            } else {
+                previous = current.getClass();
+                min++;
+            }
         }
 
         return list;
