b330caaafaf27a4066bb86c365b524761b926e82
Dmitry <ya.warkus@gmail.com>
2019-11-28 12:21:22 +0200

    Solved

--- src/JavaSyntax/lvl8/lecture11/IsDateOddDemo.java
+++ src/JavaSyntax/lvl8/lecture11/IsDateOddDemo.java
@@ -5,16 +5,18 @@ import java.util.Date;
 public class IsDateOddDemo {
     public static void main(String[] args) {
 
-        System.out.println(isDateOdd("JANUARY 1 2020"));
-        System.out.println(isDateOdd("JANUARY 2 2020"));
+        //        System.out.println(isDateOdd("JANUARY 1 2000"));
+//        System.out.println(isDateOdd("JANUARY 2 2020"));
+        System.out.println(isDateOdd("MAY 1 2013"));
 
     }
 
-    private static boolean isDateOdd(String date) {
+    public static boolean isDateOdd(String date) {
         Date dateStart = new Date(date);
         Date dateFinish = new Date(date);
         dateStart.setMonth(0);//месяц январь, нумерация для месяцев 0-11
         dateStart.setDate(1); // первое число
+
 
         long msTimeDistance = dateFinish.getTime() - dateStart.getTime();
         long msDay = 24 * 60 * 60 * 1000; // сколько миллисекунд в одних сутках
