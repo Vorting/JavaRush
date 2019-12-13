3c2f1e7a6d057092ae041a2133975ff9e096623e
Dmitry <ya.warkus@gmail.com>
2019-12-13 12:52:43 +0200

    add ListOfNumbers

--- src/JavaSyntax/lvl9/lecture11/ListOfNumbers.java
+++ src/JavaSyntax/lvl9/lecture11/ListOfNumbers.java
@@ -1,25 +1,36 @@
 package JavaSyntax.lvl9.lecture11;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ListOfNumbers {
     public static void main(String[] args) {
         List<int[]> numbersArray = createList();
-
-
+        printList(numbersArray);
     }
 
     private static List<int[]> createList() {
         List<int[]> arrayList = new ArrayList<>();
-        for ()
-        return null;
+        int[] firstArr = {1, 2, 3, 4, 5};
+        int[] secondArr = {1, 2};
+        int[] thirdArr = {1, 2, 3, 4};
+        int[] fourthArr = {1, 2, 3, 4, 5, 6, 7};
+        int[] fifthArr = {};
+
+        arrayList.add(firstArr);
+        arrayList.add(secondArr);
+        arrayList.add(thirdArr);
+        arrayList.add(fourthArr);
+        arrayList.add(fifthArr);
+        return arrayList;
     }
 
     public static void printList(List<int[]> list) {
         for (int[] array : list) {
+            System.out.println();
             for (int x : array) {
-                System.out.println(x);
+                System.out.print(x);
             }
         }
     }
