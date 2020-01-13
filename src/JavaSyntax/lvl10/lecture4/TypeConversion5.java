7e63cd73cbe19ee30f297a46886a5ed2e449e388
Dmitry <ya.warkus@gmail.com>
2020-01-13 16:48:25 +0200

    add TypeConversion5 & 6

--- src/JavaSyntax/lvl10/lecture4/TypeConversion5.java
+++ src/JavaSyntax/lvl10/lecture4/TypeConversion5.java
@@ -1,10 +1,10 @@
 package JavaSyntax.lvl10.lecture4;
 
-public class TypeConversion4 {
+public class TypeConversion5 {
     public static void main(String[] args) {
         int a = (byte) 44;
         int b = (short) 300;
-        int c = (b - a);
+        short c =(short) (b - a);
         System.out.println(c);
     }
 }
---
+++ src/JavaSyntax/lvl10/lecture4/TypeConversion6.java
@@ -0,0 +1,14 @@
+package JavaSyntax.lvl10.lecture4;
+
+public class TypeConversion6 {
+    public static void main(String[] args) {
+        double d = (short) 2.50256e2d;
+        char c = (short) 'd';
+        short s = (short) 2.22;
+        int i = 150000;
+        float f =  0.50f;
+        double result = f + (i / c) - (d * s) - 500e-3;
+
+        System.out.println(result);
+    }
+}
