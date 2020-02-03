62c4ace72b3461645aeb6ac3bc36911e1455ea19
Dmitry <ya.warkus@gmail.com>
2020-02-03 11:11:05 +0200

    add lecture11

---
+++ src/JavaSyntax/lvl10/lecture11/RightAnswerIsD.java
@@ -0,0 +1,13 @@
+package JavaSyntax.lvl10.lecture11;
+
+public class RightAnswerIsD {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        int c = 3;
+        int e = 2;
+        double d = a + b / c / (double) e;
+        System.out.println(d);
+    }
+}
