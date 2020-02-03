9379eea987623c1c7cbd4a724398d8613a30fe6f
Vorting <ya.warkus@gmail.com>
2020-02-03 20:57:21 +0200

    add InProgress

---
+++ src/JavaSyntax/lvl10/lecture11/InProgress.java
@@ -0,0 +1,10 @@
+package JavaSyntax.lvl10.lecture11;
+
+public class InProgress {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 17;
+        double d = a / b;
+        System.out.println(d);
+    }
+}
