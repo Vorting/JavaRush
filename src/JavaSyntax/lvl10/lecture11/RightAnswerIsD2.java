c13e4c8de750a5bae1fb03993d5b7d59d6b0284a
Dmitry <ya.warkus@gmail.com>
2020-02-03 14:42:21 +0200

    add RightAnswerIsD2

---
+++ src/JavaSyntax/lvl10/lecture11/RightAnswerIsD2.java
@@ -0,0 +1,13 @@
+package JavaSyntax.lvl10.lecture11;
+
+public class RightAnswerIsD2 {
+    public static void main(String[] args) {
+        int a =(byte) 257;
+        int b = 4;
+        int c = 3;
+        int e = 2;
+        double d = a + b / c / e;
+
+        System.out.println(d);
+    }
+}
