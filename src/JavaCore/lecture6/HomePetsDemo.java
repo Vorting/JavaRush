64bd49b2b156703e7bcfca50a35ea553c88ef361
Dmitry <ya.warkus@gmail.com>
2020-03-25 16:29:58 +0200

    add JavaCore

---
+++ src/JavaCore/lecture6/HomePetsDemo.java
@@ -0,0 +1,19 @@
+package JavaCore.lecture6;
+
+public class HomePetsDemo {
+    public static void main(String[] args) {
+
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+    }
+
+    public class Dog extends Pet {
+
+    }
+
+}
---
+++ src/JavaCore/level1/InheritanceDemo.java
@@ -0,0 +1,17 @@
+package JavaCore.level1;
+
+public class InheritanceDemo {
+    public static void main(String[] args) {
+
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegasus {
+
+    }
+}
+
+
