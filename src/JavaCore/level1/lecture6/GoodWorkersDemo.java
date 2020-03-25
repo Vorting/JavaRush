8dd98254d47c969fd18433dd0e7d6a2ba0b2ad41
Dmitry <ya.warkus@gmail.com>
2020-03-25 16:52:18 +0200

    add 2 files in lectur6

---
+++ src/JavaCore/level1/lecture6/GoodWorkersDemo.java
@@ -0,0 +1,20 @@
+package JavaCore.level1.lecture6;
+
+public class GoodWorkersDemo {
+    public static void main(String[] args) {
+
+    }
+
+    public class Employee {
+    }
+
+    public class Manager extends Employee {
+    }
+
+    public class Chief extends Employee {
+    }
+
+    public class Secretary extends Employee {
+    }
+
+}
---
+++ src/JavaCore/level1/lecture6/ITCompanyDemo.java
@@ -0,0 +1,36 @@
+package JavaCore.level1.lecture6;
+
+public class ITCompanyDemo {
+    public static void main(String[] args) {
+
+    }
+
+    public class Worker {
+    }
+
+    public class IT extends Worker {
+    }
+
+    public class Programmer extends IT {
+    }
+
+    public class ProjectManager extends IT {
+    }
+
+    public class CTO extends IT {
+    }
+
+    public class Clerk extends Worker {
+    }
+
+    public class HR extends Clerk {
+    }
+
+    public class Cleaner extends Clerk {
+    }
+
+    public class OfficeManager extends Clerk {
+    }
+
+
+}
