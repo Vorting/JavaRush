1fc676455f6b0054fe6611443f9e113d6b35a052
Dmitry <ya.warkus@gmail.com>
2020-03-25 14:59:12 +0200

    solved alphabetDemo

---
+++ src/JavaSyntax/lvl10/lecture11/AlphabetDemo.java
@@ -0,0 +1,41 @@
+package JavaSyntax.lvl10.lecture11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AlphabetDemo {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        // Алфавит
+        List<Character> alphabet = Arrays.asList(
+                'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж',
+                'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о',
+                'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц',
+                'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я');
+
+        // Ввод строк
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < 10; i++) {
+            String s = reader.readLine();
+            list.add(s.toLowerCase());
+        }
+
+        int increase = 0;
+        for (char abc : alphabet) {
+            for (String readString : list) {
+                for (char symbol : readString.toCharArray()) {
+                    if (abc == symbol) {
+                        increase++;
+                    }
+                }
+                System.out.println(abc + " " + increase);
+                increase = 0;
+            }
+        }
+    }
+}
